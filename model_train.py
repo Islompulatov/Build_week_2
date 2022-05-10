beeb0081a631b5963858bd84205fdd6a6bcd87c9
omolara-atoyebi <98391179+omolara-atoyebi@users.noreply.github.com>
2022-05-10 16:56:11 +0300

    update

--- model_train.py
+++ model_train.py
@@ -17,74 +17,80 @@ from sklearn.svm import SVC
 from sklearn import metrics, pipeline
 import time
 from sklearn import metrics
-five_second_data = pd.read_csv("./dataset_5secondWindow/dataset_5secondWindow.csv")
+# five_second_data = pd.read_csv("./dataset_5secondWindow/dataset_5secondWindow.csv")
 
-not_null_col = [col for col in five_second_data.columns if five_second_data[col].isnull().sum()<800]
-new_data = pd.DataFrame(five_second_data[not_null_col])
-print(new_data.head())
-
+# not_null_col = [col for col in five_second_data.columns if five_second_data[col].isnull().sum()<800]
+# new_data = pd.DataFrame(five_second_data[not_null_col])
+# print(new_data.head())
 OE = OrdinalEncoder()
-ct = np.asarray(new_data['target'])
-new_data['target'] = OE.fit_transform(ct.reshape(-1,1))
+# ct = np.asarray(new_data['target'])
+# new_data['target'] = OE.fit_transform(ct.reshape(-1,1))
 
 
-ct2 = np.asarray(new_data['user'])
-new_data['user'] = OE.fit_transform(ct2.reshape(-1,1))
-new_data = new_data.sort_values(by='user', ascending=True)
-print(new_data)
+# print(new_data)
+df = pd.read_csv("./fine1_dataset.csv")
+
+ct2 = np.asarray(df['user'])
+df['user'] = OE.fit_transform(ct2.reshape(-1,1))
+df = df.sort_values(by='user', ascending=True)
+#print(df)
 
 # df = pd.read_csv("./final_dataset.csv")
 # #print(df.shape)
-# #split original data into train and test
-# df_train = df.iloc[:4714,:]
-# df_test = df.iloc[4714:,:]
-# #print(df_test.shape)
+#split original data into train and test
+df_train = df.iloc[:4714,:]
+df_test = df.iloc[4714:,:]
+#print(df_train)
 
-# #Train set 
-# X_train = df_train.drop(['target'],axis=1)
-# y_train = df_train['target']
+#Train set 
+X_train = df_train.drop(['target','user'],axis=1)
+#removing id column and unnamed column. not a feature needed for the training
+X_train = X_train.iloc[:,2:]
 
-# # Test set 
+y_train = df_train['target']
+#print(X_train)
+# Test set 
 
-# x_test = df_test.drop(['target'],axis =1)
-# y_test = df_test['target']
+x_test = df_test.drop(['target','user'],axis =1)
+x_test= x_test.iloc[:,2:]
+y_test = df_test['target']
 
-# #scaled the features
-# SC = StandardScaler()
-# X_train = SC.fit_transform(X_train)
-# x_test = SC.transform(x_test)
-# #print(X_train)
+#scaled the features
+SC = StandardScaler()
+X_train = SC.fit_transform(X_train)
+x_test = SC.transform(x_test)
+#print(X_train)
 
-# tree_classifiers = {
-#   "Decision Tree": DecisionTreeClassifier(),
-#   "Extra Trees":ExtraTreesClassifier(),
-#   "Random Forest":RandomForestClassifier(),
-#   "AdaBoost":AdaBoostClassifier(),
-#   "Skl GBM": GradientBoostingClassifier(),
-#   "Skl HistGBM":HistGradientBoostingClassifier(),
-#   "XGBoost": XGBClassifier(),
-#   "LightGBM":LGBMClassifier(),
-#   "CatBoost": CatBoostClassifier(),
-#   "SVM":      SVC()}
-# tree_classifiers = {name: pipeline.make_pipeline( model) for name, model in tree_classifiers.items()}
+tree_classifiers = {
+  "Decision Tree": DecisionTreeClassifier(),
+  "Extra Trees":ExtraTreesClassifier(),
+  "Random Forest":RandomForestClassifier(),
+  "AdaBoost":AdaBoostClassifier(),
+  "Skl GBM": GradientBoostingClassifier(),
+  "Skl HistGBM":HistGradientBoostingClassifier(),
+  "XGBoost": XGBClassifier(),
+  "LightGBM":LGBMClassifier(),
+  "CatBoost": CatBoostClassifier(),
+  "SVM":      SVC()}
+tree_classifiers = {name: pipeline.make_pipeline( model) for name, model in tree_classifiers.items()}
 
-# results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': [], 'Time': []})
-# for model_name, model in tree_classifiers.items():
+results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': [], 'Time': []})
+for model_name, model in tree_classifiers.items():
     
-#     start_time = time.time()
-#     model.fit(X_train, y_train)
-#     total_time = time.time() - start_time
+    start_time = time.time()
+    model.fit(X_train, y_train)
+    total_time = time.time() - start_time
         
-#     pred = model.predict(x_test)
+    pred = model.predict(x_test)
     
-#     results = results.append({"Model":    model_name,
-#                               "Accuracy": metrics.accuracy_score(y_test, pred)*100,
-#                               "Bal Acc.": metrics.balanced_accuracy_score(y_test, pred)*100,
-#                               "Time":     total_time},
-#                               ignore_index=True)
+    results = results.append({"Model":    model_name,
+                              "Accuracy": metrics.accuracy_score(y_test, pred)*100,
+                              "Bal Acc.": metrics.balanced_accuracy_score(y_test, pred)*100,
+                              "Time":     total_time},
+                              ignore_index=True)
                               
                               
-# results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
-# # results_ord.index += 1 
-# # results_ord.style.bar(subset=['Accuracy', 'Bal Acc.'], vmin=0, vmax=100, color='#5fba7d')
-# print(results_ord)
+results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
+# results_ord.index += 1 
+# results_ord.style.bar(subset=['Accuracy', 'Bal Acc.'], vmin=0, vmax=100, color='#5fba7d')
+print(results_ord)
